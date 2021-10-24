1cd1a3ccca0c6009167eb30f1061d5e9487cb360
Justin Ly <justinly15@gmail.com>
2021-10-24 15:35:05 -0400

    removal

--- call_emission.py
+++ call_emission.py
@@ -1,20 +1,20 @@
 import sys
 # PyQt5中使用的基本控件都在PyQt5.QtWidgets模块中
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QDialog
 # 导入designer工具生成的login模块
 from emission import Ui_Dialog
 from get_distance import calculate_distance
 from get_emissions import calculate_emissions
 
 
-class MyMainForm(QMainWindow, Ui_Dialog):
+class MyMainForm(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         super(MyMainForm, self).__init__(parent)
         self.setupUi(self)
         # 添加登录按钮信号和槽。注意display函数不加小括号()
-        self.login_Button.clicked.connect(self.display)
+        self.buttonBox.clicked.connect(self.display)
         # 添加退出按钮信号和槽。调用close函数
-        self.cancel_Button.clicked.connect(self.close)
+        #self.cancel_Button.clicked.connect(self.close)
 
     def display(self):
         # use text Edit.text()to get input"":::::::::::::::"
@@ -31,9 +31,9 @@ class MyMainForm(QMainWindow, Ui_Dialog):
 if __name__ == "__main__":
     # PyQt5 app needs QApplication object. sys.argv是命令行参数列表，确保程序可以双击运行
     app = QApplication(sys.argv)
-    # initialize
+    # intialize
     myWin = MyMainForm()
     # show widgets on screen
     myWin.show()
-    # run app, sys.exit method makes sure the app exit completely
+    # run app, sys.exit method makes sure the app exeit completely
     sys.exit(app.exec_())
