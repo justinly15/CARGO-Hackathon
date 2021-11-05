c75f0d914a3b33d9029fef3c4ae055d1e1bb032a
Yuqi29 <92478707+Yuqi29@users.noreply.github.com>
2021-11-05 12:47:43 -0400

    Delete redundant code and add annotations

--- call_emission.py
+++ call_emission.py
@@ -1,8 +1,6 @@
 import sys
-# PyQt5中使用的基本控件都在PyQt5.QtWidgets模块中
 from PyQt5.QtWidgets import QApplication, QMainWindow
-# 导入designer工具生成的login模块
-from emission import Ui_MainWindow
+from emission import Ui_MainWindow # import the GUI construction .py file
 from get_distance import calculate_distance
 from get_emissions import calculate_emissions
 
@@ -11,24 +9,25 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainForm, self).__init__(parent)
         self.setupUi(self)
-        # 添加登录按钮信号和槽。注意display函数不加小括号()
+        # add 
         self.pushButton.clicked.connect(self.display)
         # 添加退出按钮信号和槽。调用close函数
         # self.buttonBox.clicked.connect(self.close)
 
     def display(self):
         # use text Edit.text()to get input"":::::::::::::::"
+        # read text from textEdit controls
         origin_text = self.textEdit_origin.toPlainText()
         dest_text = self.textEdit_dest.toPlainText()
         mass_text = self.textEdit_mass.toPlainText()
         mode_text = self.comboBox_mode.currentText()
         dist = calculate_distance(origin_text, dest_text)
         emission_text = calculate_emissions(dist, mode_text, mass_text)
-        # 利用text Browser控件对象setText()函数设置界面显示
+        # display text using text Browser controls' object setText()
         self.textBrowser_emission.setText(emission_text)
 
 if __name__ == "__main__":
-    # PyQt5 app needs QApplication object. sys.argv是命令行参数列表，确保程序可以双击运行
+    # PyQt5 app needs QApplication object. sys.argv is command line parameters list, to ensure that the program can be run by double-clicking
     app = QApplication(sys.argv)
     # intialize
     myWin = MyMainForm()
